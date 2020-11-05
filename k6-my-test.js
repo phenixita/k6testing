266b7191f732a809b0c5c8c90d0b98e97693f936
Michele <michele.ferracin@live.it>
2020-11-05 17:24:43 +0100

    Update k6-my-test.js

--- k6-my-test.js
+++ k6-my-test.js
@@ -2,22 +2,26 @@ import http from 'k6/http';
 import { check, group, sleep, fail } from 'k6';
 
 export let options = {
-  stages: [
-    { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes
-    { duration: '5m', target: 0 }, // ramp-down to 0 users
-  ],
+  vus: 1, // 1 user looping for 1 minute
+  duration: '1m',
+
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
-    'listed work items': ['p(99)<1500'], // 99% of requests must complete below 1.5s
   },
 };
  
 
-export default () => { 
-  let response = http.get(`https://owp-appservice.azurewebsites.net/Workitems/Index`);
+export default () => {
+
+  let response = http.get(`https://owp-appservice.azurewebsites.net/`);
+
+  sleep(1);
+
+  response = http.get(`https://owp-appservice.azurewebsites.net/Workitems/Index`);
   check(response, {
     'status is 200': (r) => r.status == 200,
   })
+
+
   sleep(1);
 };
