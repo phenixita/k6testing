5bef4156641e807a85d0efbe8fce1bb4b765082d
Michele <michele.ferracin@live.it>
2020-11-05 17:21:31 +0100

    Fix error in js code

--- k6-my-test.js
+++ k6-my-test.js
@@ -15,7 +15,7 @@ export let options = {
  
 
 export default () => { 
-  response = http.get(`https://owp-appservice.azurewebsites.net/Workitems/Index`);
+  let response = http.get(`https://owp-appservice.azurewebsites.net/Workitems/Index`);
   check(response, {
     'status is 200': (r) => r.status == 200,
   })
